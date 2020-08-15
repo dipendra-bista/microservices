97ed835d048e8f3170b2b803e666ff0912ea132a
punoj <punojbista44@gmail.com>
2020-08-15 18:48:47 +0545

    connecting config client to cloud config server

--- spring-cloud-config-server/src/main/java/com/punojsoft/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/punojsoft/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
