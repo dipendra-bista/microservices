c0675667ef25c467dffdce63a07b4c5e65612e4f
punoj <punojbista44@gmail.com>
2020-08-16 11:54:55 +0545

    invoking currency-exchange-service from currency-conversion-serivice using RestTemplate

--- currency-conversion-service/src/main/java/com/punojsoft/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/punojsoft/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,15 +1,35 @@
 package com.punojsoft.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
-    @GetMapping("corrency-converter/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, BigDecimal quantity) {
-        return new CurrencyConversionBean(1, from, to, BigDecimal.ONE, quantity, quantity, 0);
+    @GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        Map<String, String> uriVaraibles = new HashMap<>();
+        uriVaraibles.put("from", from);
+        uriVaraibles.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> response = new RestTemplate().
+                getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversionBean.class
+                        , uriVaraibles);
+        CurrencyConversionBean currencyConversionBean = response.getBody();
+
+        return new CurrencyConversionBean(1,
+                from,
+                to,
+                currencyConversionBean.getConversionMultiple(),
+                quantity,
+                quantity.multiply(currencyConversionBean.getConversionMultiple()), currencyConversionBean.getPort());
     }
 }
