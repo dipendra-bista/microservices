8db9a0a905fa368dfe9c4e5e08144f44aaf4c678
punoj <punojbista44@gmail.com>
2020-08-16 17:09:45 +0545

    Zull api gateway logging implementation

---
+++ netflix-zull-api-gateway-server/src/main/java/com/punojsoft/microservices/netflixzullapigatewayserver/NetflixZullApiGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.punojsoft.microservices.netflixzullapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class NetflixZullApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZullApiGatewayServerApplication.class, args);
+    }
+
+}
---
+++ netflix-zull-api-gateway-server/src/test/java/com/punojsoft/microservices/netflixzullapigatewayserver/NetflixZullApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punojsoft.microservices.netflixzullapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZullApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
